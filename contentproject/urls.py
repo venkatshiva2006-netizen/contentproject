430354c1278a78c5f08c111fb1a963c528453321
Venkat <venkatshiva2006@gmail.com>
2025-11-16 21:43:05 +0530

    Added homepage redirect

--- contentproject/urls.py
+++ contentproject/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', lambda request: redirect('add_content')),
     path('', include('contentapp.urls')),
 ]
 
